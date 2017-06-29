dbc94a4eccfbbbf1de9049201dcaaccb21c57e33
Breno Oliveira dos Santos <breno.santos@tmax.com.br>
2017-06-29 15:42:50 -0300

    services configurados

--- Greeter.cs
+++ Greeter.cs
@@ -1,7 +1,25 @@
+using Microsoft.Extensions.Configuration;
+
 namespace OdeToFood
 {
-    public class Greeter
+
+    public interface IGreeter
     {
-        
+        string GetGreeting();    
+    }
+
+    public class Greeter : IGreeter
+    {
+
+        private string _greeting;
+
+        public Greeter(IConfiguration configration)
+        {
+            _greeting = configration["Greeting"];
+        }
+
+        public string GetGreeting(){
+            return _greeting;
+        }
     }
 }
